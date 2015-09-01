8c2fc610355aae8fda67aa9fa7cdeca5d41a658b
Aftermaths <fnzzhou@gmail.com>
2015-09-01 15:46:39 +0100

    add restaurant feature complete

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,22 @@
 class RestaurantsController < ApplicationController
   def index
-    # fail 'Hello from the index action'
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
   end
 end
