a907f645ebb1769fd138d1e7cc09115b90f2862b
Aftermaths <fnzzhou@gmail.com>
2015-09-03 12:12:35 +0100

    facebook login added

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  root to: "restaurants#index"
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   # root 'welcome#index'
 
   # Example of regular route:
